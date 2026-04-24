c366c788719f2076e1445faf98e1cc4f9f4e4d87
Illia M. <illia.masakov@gmail.com>
2026-04-24 18:58:35 +0300

    refactor getNumber to use recurse function

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
-/**
- * @param {numbet} target
- * @param {number} current
- * @returns getting a number
- */
-function getNumber(target, current = 1) {
-    if (current === target) return "1";
-    if (current > target) return null;
+function getNumber(target, start = 1) {
+    function recurse(current, expr) {
+        if (current === target) return expr;
+        if (current > target) return null;
 
-    const multiply = getNumber(target, current * 3);
-    if (multiply !== null) return `(${multiply} * 3)`;
+        const multiply = recurse(current * 3, `(${expr} * 3)`);
+        if (multiply !== null) return multiply;
 
-    const add = getNumber(target, current + 5);
-    if (add !== null) return `(${add} + 5)`;
+        const add = recurse(current + 5, `(${expr} + 5)`);
+        if (add !== null) return add;
 
-    return null;
+        return null;
+    }
+    return recurse(start, `${start}`);
 }
-
 console.log(getNumber(18));
